08da538944eae5b967f903429c2fa1b68ff19179
Daniel Winters <danielwinters@live.com>
2018-02-03 22:22:51 +0000

    Update search.js

--- js/search.js
+++ js/search.js
@@ -23,7 +23,7 @@ function search() {
     // Uses the JavaScript client library to create a search.list() API call.
     var request = gapi.client.youtube.search.list({
         part: 'snippet',
-        videoDuration: 'dessa',
+        videoDuration: 'short',
         maxResults: 1
     });
     
