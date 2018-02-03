005db50dc31b65ecac571026b2e3340aa7f97b65
Daniel Winters <danielwinters@live.com>
2018-02-03 22:56:11 +0000

    Update search.js

--- js/search.js
+++ js/search.js
@@ -5,7 +5,7 @@
 function showResponse(response) {
     var responseString = JSON.stringify(response, '', 2);
     document.getElementById('response').innerHTML += responseString;
-    document.getElementById('video').innerHTML += response.items.kind;
+    document.getElementById('video').innerHTML += response.items[0].kind;
 
 }
 
