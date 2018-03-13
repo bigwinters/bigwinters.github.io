251b61d66d4f9b045ee9d323407d4ad671635e9d
Daniel Winters <danielwinters@live.com>
2018-03-13 14:23:18 +0000

    Update search.js

--- js/search.js
+++ js/search.js
@@ -51,6 +51,6 @@ input.addEventListener("keyup", function(event) {
   // Number 13 is the "Enter" key on the keyboard
   if (event.keyCode === 13) {
     // Trigger the button element with a click
-    document.getElementById("searchButton").click();
+    search();
   }
 });
